2996bd407883069de4cef08778d1060f4a1b6d8b
João Álvaro Oliveira Silva João <joaosilvaabc1@gmail.com>
2023-09-06 16:17:28 -0300

    Scroll Reveal e Jquery Mask

---
+++ Desafio 3/assets/js/script.js
@@ -0,0 +1,8 @@
+// Scroll Reveal
+ScrollReveal().reveal('.cards', { interval: 200 });
+ScrollReveal().reveal('form', { delay: 200 });
+
+// Form Mask - Telefone
+$(document).ready(function(){
+    $('.telefone').mask('(00) 00000-0000');
+});
